1635bc725122c121f66171deafae6e92f64b2f4e
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-23 19:25:04 +0100

    quotes componnets done and completed

--- src/components/HighlightedQuote/index.js
+++ src/components/HighlightedQuote/index.js
@@ -1,1 +1,10 @@
 import classes from "./style.module.css";
+
+export const HighlightedQuote = (props) => {
+  return (
+    <figure className={classes.quote}>
+      <p>{props.text}</p>
+      <figcaption>{props.author}</figcaption>
+    </figure>
+  );
+};
--- src/components/QuoteItem/index.js
+++ src/components/QuoteItem/index.js
@@ -1,1 +1,19 @@
 import classes from "./style.module.css";
+
+import { Link } from "react-router-dom";
+
+export const QuoteItem = (props) => {
+  return (
+    <li className={classes.item}>
+      <figure>
+        <blockquote>
+          <p>{props.text}</p>
+        </blockquote>
+        <figcaption>{props.author}</figcaption>
+      </figure>
+      <Link className="btn" to={`/quotes/${props.id}`}>
+        View Fullscreen
+      </Link>
+    </li>
+  );
+};
