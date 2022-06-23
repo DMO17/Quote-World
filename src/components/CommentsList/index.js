b3c5f27863c77b59fa9a66ecae0458a2abfc32cc
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-23 19:47:25 +0100

    all pages defined

--- src/components/CommentsList/index.js
+++ src/components/CommentsList/index.js
@@ -1,1 +1,13 @@
 import classes from "./style.module.css";
+
+import CommentItem from "../CommentItem";
+
+const CommentsList = (props) => {
+  return (
+    <ul className={classes.comments}>
+      {props.comments.map((comment) => (
+        <CommentItem key={comment.id} text={comment.text} />
+      ))}
+    </ul>
+  );
+};
